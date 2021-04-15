7d4326c71f9053e0fbd2ca032b403aa72105651e
Nicolás Salvanés <nicolas.salvanes@mercadolibre.com>
2021-04-15 02:35:22 -0300

    Update card payment example with SDK JS v2 and cardForm feature

--- client/js/index.js
+++ client/js/index.js
@@ -1,175 +1,145 @@
- //REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
-window.Mercadopago.setPublishableKey("YOUR_PUBLIC_KEY");
+//REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://www.mercadopago.com/developers/panel
+const mp = new MercadoPago('YOUR_PUBLIC_KEY');
 
-window.Mercadopago.getIdentificationTypes();
-  
-document.getElementById('cardNumber').addEventListener('change', guessPaymentMethod);
-function guessPaymentMethod(event) {
-    cleanCardInfo();
+function loadCardForm() {
+    const productCost = document.getElementById('amount').value;
+    const productDescription = document.getElementById('product-description').innerText;
 
-    let cardnumber = document.getElementById("cardNumber").value;
-    if (cardnumber.length >= 6) {
-        let bin = cardnumber.substring(0,6);
-        window.Mercadopago.getPaymentMethod({
-            "bin": bin
-        }, setPaymentMethod);
-    }
+    const cardForm = mp.cardForm({
+        amount: productCost,
+        autoMount: true,
+        form: {
+            id: "form-checkout",
+            cardholderName: {
+                id: "form-checkout__cardholderName",
+                placeholder: "Holder name",
+            },
+            cardholderEmail: {
+                id: "form-checkout__cardholderEmail",
+                placeholder: "E-mail",
+            },
+            cardNumber: {
+                id: "form-checkout__cardNumber",
+                placeholder: "Card number",
+            },
+            cardExpirationMonth: {
+                id: "form-checkout__cardExpirationMonth",
+                placeholder: "MM",
+            },
+            cardExpirationYear: {
+                id: "form-checkout__cardExpirationYear",
+                placeholder: "YY",
+            },
+            securityCode: {
+                id: "form-checkout__securityCode",
+                placeholder: "Security code",
+            },
+            installments: {
+                id: "form-checkout__installments",
+                placeholder: "Installments",
+            },
+            identificationType: {
+                id: "form-checkout__identificationType",
+            },
+            identificationNumber: {
+                id: "form-checkout__identificationNumber",
+                placeholder: "Identification number",
+            },
+            issuer: {
+                id: "form-checkout__issuer",
+                placeholder: "Issuer",
+            },
+        },
+        callbacks: {
+            onFormMounted: error => {
+                if (error) 
+                    return console.warn("Form Mounted handling error: ", error);
+                console.log("Form mounted");
+            },
+            onSubmit: event => {
+                event.preventDefault();
+            
+                const {
+                    paymentMethodId,
+                    issuerId,
+                    cardholderEmail: email,
+                    amount,
+                    token,
+                    installments,
+                    identificationNumber,
+                    identificationType,
+                } = cardForm.getCardFormData();
+            
+                fetch("/process_payment", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        token,
+                        issuerId,
+                        paymentMethodId,
+                        transactionAmount: Number(amount),
+                        installments: Number(installments),
+                        description: productDescription,
+                        payer: {
+                            email,
+                            identification: {
+                                type: identificationType,
+                                number: identificationNumber,
+                            },
+                        },
+                    }),
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(result => {
+                    document.getElementById("payment-status").innerText = result.status;
+                    document.getElementById("payment-detail").innerText = result.message;
+                    $('.container__payment').fadeOut(500);
+                    setTimeout(() => { $('.container__result').show(500).fadeIn(); }, 500);
+                })
+                .catch(error => {
+                    alert("Unexpected error\n"+JSON.stringify(error));
+                });;
+            },
+            onFetching: (resource) => {
+              console.log("Fetching resource: ", resource);
+              const payButton = document.getElementById("form-checkout__submit");
+              payButton.setAttribute('disabled', true);
+              return () => {
+                payButton.removeAttribute("disabled");
+              };
+            },
+        },
+    });
 };
-
-function setPaymentMethod(status, response) {
-    if (status == 200) {
-        let paymentMethod = response[0];
-        
-        document.getElementById('paymentMethodId').value = paymentMethod.id;
-        document.getElementById('cardNumber').style.backgroundImage = 'url(' + paymentMethod.thumbnail + ')';
-        
-        if(paymentMethod.additional_info_needed.includes("issuer_id")){
-            getIssuers(paymentMethod.id);
-
-        } else {
-            document.getElementById('issuerInput').classList.add("hidden");
-
-            getInstallments(
-                paymentMethod.id,
-                document.getElementById('amount').value
-            );
-        }
-
-    } else {
-        alert(`payment method info error: ${response}`);
-    }
-}
-
-function getIssuers(paymentMethodId) {
-    window.Mercadopago.getIssuers(
-        paymentMethodId, 
-        setIssuers
-    );
-}
-
-function setIssuers(status, response) {
-    if (status == 200) {
-        let issuerSelect = document.getElementById('issuer');
-
-        response.forEach( issuer => {
-            let opt = document.createElement('option');
-            opt.text = issuer.name;
-            opt.value = issuer.id;
-            issuerSelect.appendChild(opt);
-        });
-        
-        if(issuerSelect.options.length <= 1){
-            document.getElementById('issuerInput').classList.add("hidden");
-        } else {
-            document.getElementById('issuerInput').classList.remove("hidden");
-        }
-        
-        getInstallments(
-            document.getElementById('paymentMethodId').value,
-            document.getElementById('amount').value,
-            issuerSelect.value
-        );
-
-    } else {
-        alert(`issuers method info error: ${response}`);
-    }
-}
-
-function getInstallments(paymentMethodId, amount, issuerId){
-    window.Mercadopago.getInstallments({
-        "payment_method_id": paymentMethodId,
-        "amount": parseFloat(amount),
-        "issuer_id": issuerId ? parseInt(issuerId) : undefined
-    }, setInstallments);
-}
-
-function setInstallments(status, response){
-    if (status == 200) {
-        document.getElementById('installments').options.length = 0;
-        response[0].payer_costs.forEach( payerCost => {
-            let opt = document.createElement('option');
-            opt.text = payerCost.recommended_message;
-            opt.value = payerCost.installments;
-            document.getElementById('installments').appendChild(opt);
-        });
-    } else {
-        alert(`installments method info error: ${response}`);
-    }
-}  
-
-//Update offered installments when issuer changes
-document.getElementById('issuer').addEventListener('change', updateInstallmentsForIssuer);
-function updateInstallmentsForIssuer(event) {
-    window.Mercadopago.getInstallments({
-        "payment_method_id": document.getElementById('paymentMethodId').value,
-        "amount": parseFloat(document.getElementById('amount').value),
-        "issuer_id": parseInt(document.getElementById('issuer').value)
-    }, setInstallments);
-}
-
-//Proceed with payment
-doSubmit = false;
-document.getElementById('paymentForm').addEventListener('submit', getCardToken);
-function getCardToken(event){
-    event.preventDefault();
-    if(!doSubmit){
-        let $form = document.getElementById('paymentForm');
-        window.Mercadopago.createToken($form, setCardTokenAndPay);
-
-        return false;
-    }
-};
-
-function setCardTokenAndPay(status, response) {
-    if (status == 200 || status == 201) {
-        let form = document.getElementById('paymentForm');
-        let card = document.createElement('input');
-        card.setAttribute('name', 'token');
-        card.setAttribute('type', 'hidden');
-        card.setAttribute('value', response.id);
-        form.appendChild(card);
-        doSubmit=true;
-        form.submit(); //Submit form data to your backend
-    } else {
-        alert("Verify filled data!\n"+JSON.stringify(response, null, 4));
-    }
-};
-
-/***
- * UX functions 
- */
-
-function cleanCardInfo() {
-    document.getElementById('cardNumber').style.backgroundImage = '';
-    document.getElementById('issuerInput').classList.add("hidden");
-    document.getElementById('issuer').options.length = 0;
-    document.getElementById('installments').options.length = 0;
-}
 
 //Handle transitions
 document.getElementById('checkout-btn').addEventListener('click', function(){ 
-    $('.shopping-cart').fadeOut(500);
-    setTimeout(() => { $('.container_payment').show(500).fadeIn(); }, 500);
+    $('.container__cart').fadeOut(500);
+    setTimeout(() => { 
+        loadCardForm();
+        $('.container__payment').show(500).fadeIn(); 
+    }, 500);
 });
 document.getElementById('go-back').addEventListener('click', function(){ 
-    $('.container_payment').fadeOut(500);
-    setTimeout(() => { $('.shopping-cart').show(500).fadeIn(); }, 500);
+    $('.container__payment').fadeOut(500);
+    setTimeout(() => { $('.container__cart').show(500).fadeIn(); }, 500);
 });
 
 //Handle price update
 function updatePrice(){
     let quantity = document.getElementById('quantity').value;
-    let unitPrice = document.getElementById('unit-price').innerHTML;
+    let unitPrice = document.getElementById('unit-price').innerText;
     let amount = parseInt(unitPrice) * parseInt(quantity);
 
-    document.getElementById('cart-total').innerHTML = '$ ' + amount;
-    document.getElementById('summary-price').innerHTML = '$ ' + unitPrice;
-    document.getElementById('summary-quantity').innerHTML = quantity;
-    document.getElementById('summary-total').innerHTML = '$ ' + amount;
+    document.getElementById('cart-total').innerText = '$ ' + amount;
+    document.getElementById('summary-price').innerText = '$ ' + unitPrice;
+    document.getElementById('summary-quantity').innerText = quantity;
+    document.getElementById('summary-total').innerText = '$ ' + amount;
     document.getElementById('amount').value = amount;
 };
 document.getElementById('quantity').addEventListener('change', updatePrice);
 updatePrice();
-
-//Retrieve product description
-document.getElementById('description').value = document.getElementById('product-description').innerHTML;
--- server/node/README.md
+++ server/node/README.md
@@ -4,12 +4,12 @@
 
 ### Requirements
 - Node v10 or higher
-- Read our [testing instructions](https://developers.mercadopago.com/en/guides/payments/api/testing)
+- Read our [testing instructions](https://www.mercadopago.com/developers/en/guides/payments/api/testing)
 - Setup your credentials: 
   - Public Key on client-side [`index.js`](https://github.com/mercadopago/card-payment-sample/tree/master/client/js/index.js#L2)
   - Private Access Token on server-side [`server.js`](https://github.com/mercadopago/card-payment-sample/tree/master/server/node/server.js#L6)
 
 ### How to run it
 - npm install
-- npm start
+- npm run start
 - Navigate to http://localhost:8080 on your browser
